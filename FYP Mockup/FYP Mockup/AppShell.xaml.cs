e22ac0f1faec78866a96ad2e95e2cbe639eee08d
Enzo Joaquin Itona Erfe <enzo@mysoftinn.com>
2021-03-24 15:30:47 +0800

    Initial Commit +Added Base Program

---
+++ FYP Mockup/FYP Mockup/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FYP_Mockup.ViewModels;
+using FYP_Mockup.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FYP_Mockup
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ FYP Mockup/FYP Mockup/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FYP_Mockup.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FYP_Mockup.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
